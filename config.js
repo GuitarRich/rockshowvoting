0674991fb4772978278cee9199b1203dddbe4c65
Richard Seal <richyseal@gmail.com>
2026-08-13 12:17:58 -0400

    Add api url

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 //
 // Leave it empty and both pages fall back to manual copy/paste.
 // ---------------------------------------------------------------------------
-window.SETLIST_API = "";
+window.SETLIST_API = "https://script.google.com/macros/s/AKfycbwIQHBalbS328VBna-L25yR1O0WTtr_fGZj2BzpO2Mw_Lr6AuNOnPxxjTjxKiNhx2x6/exec";
 
 // The people voting. Must match the column headers in the sheet (row 3, G..M).
 window.SETLIST_VOTERS = ["Rich", "Ashley", "CJ", "Justin", "Isaac", "Julie", "Organiser"];
