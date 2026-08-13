5b959e0ab2436a6ee91840e5bb0fb34fedb4df96
Richard Seal <richyseal@gmail.com>
2026-08-13 14:21:57 -0400

    Update api

--- config.js
+++ config.js
@@ -6,7 +6,7 @@
 //
 // Leave it empty and both pages fall back to manual copy/paste.
 // ---------------------------------------------------------------------------
-window.SETLIST_API = "https://script.google.com/macros/s/AKfycbwxy7MBcA-lpdsNGRzuzAsUteFusJXS1vr-84iM_upc2z9v_Ph2iLbHBLbb624NSZKj1Q/exec";
+window.SETLIST_API = "https://script.google.com/macros/s/AKfycbyH7ttFTxekXX_0MjHh8K4uZ5EKbZyA6RlvnHBSsk0C1IvhOFgOEn-6jLQKsH2iSsC3/exec";
 
 
 // The people voting. Must match the column headers in the sheet (row 3, G..M).
